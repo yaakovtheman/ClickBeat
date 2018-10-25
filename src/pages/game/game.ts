789b81b5e1179f00d2202a1b4ee2cb94c0f04cb8
yaakovtheman <yaakovtheman@gmail.com>
2018-10-25 19:26:57 +0300

    game added

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -9,22 +9,56 @@ import {NativeAudio} from "@ionic-native/native-audio";
 export class GamePage {
 
   constructor(public navCtrl: NavController,private nativeAudio: NativeAudio,private platform : Platform) {
-    this.nativeAudio.preloadSimple('heartbeat', 'assets/mp3/heartbeat.mp3').then(()=>{});
+    this.nativeAudio.preloadComplex('heartbeatComplex', 'assets/mp3/heartbeat.mp3', 1, 10, 0).then(()=>{});
   }
   timerId: number;
+  simpleId: number;
+  complexId: number;
   beats: number;
   count: number = 0;
+  imgClass: string;
+  gameRes: number;
+  resText: string;
+  resNum: number;
+  isDis: boolean = true;
 
   getRandomNumber(): number{
-    return Math.floor(Math.random() * 190) + 40  ;
+    return Math.floor(Math.random() * 180) + 40;
   }
   startTest(): void{
+    clearInterval(this.timerId);
+    this.resText = "";
+    this.resNum = null;
+    this.isDis = false;
     this.beats = this.getRandomNumber();
     this.timerId = setInterval(() => {
-      this.nativeAudio.play('heartbeat').then(()=>{this.count++});
+      this.animate();
+      this.nativeAudio.play('heartbeatComplex').then(()=>{this.count++});
     }, (60/this.beats)*1000);
-    this.nativeAudio.play('heartbeat').then(()=>{});
+    this.nativeAudio.play('heartbeatComplex').then(()=>{});
+  }
+  stopPlay(){
+    clearInterval(this.complexId);
+    clearInterval(this.timerId);
+  }
+  animate() {
+    this.imgClass = "img-ani";
 
+    // this will execute async after 100ms
+    setTimeout(() => {
+      this.imgClass = "animated bounce";
+    }, 100);
+  }
+  checkResult(){
+    this.stopPlay();
+    let num = Math.abs(this.beats - this.gameRes);
+    if (num <= 8){
+      this.resText = " כל הכבוד! ";
+    }else {
+      this.resText = " נסה שוב! " ;
+    }
+    this.isDis = true;
+    this.resNum = this.beats
   }
 
 }
